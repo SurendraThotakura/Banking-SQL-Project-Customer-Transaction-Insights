aaedc6ea85469c5c348b385e838b701f17505d73
SurendraThotakura <surendrathotakura95@gmail.com>
2025-05-27 12:55:03 -0500

    Update Query.sql

--- Query.sql
+++ Query.sql
@@ -1,1 +1,13 @@
-
+-- high_value_customers.sql
+SELECT 
+    c.customer_id,
+    CONCAT(c.first_name, ' ', c.last_name) AS full_name,
+    TO_CHAR(t.transaction_date, 'YYYY-MM') AS month,
+    ROUND(AVG(t.amount), 2) AS avg_transaction_amount
+FROM customers c
+JOIN transactions t ON c.customer_id = t.customer_id
+GROUP BY c.customer_id, full_name, TO_CHAR(t.transaction_date, 'YYYY-MM')
+HAVING 
+    COUNT(t.transaction_id) > 5 AND
+    AVG(t.amount) > 5000
+ORDER BY avg_transaction_amount DESC;
